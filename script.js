d22376f1b31d954ca0c7a7dadeb79001b9f6a294
ahmadou18 <ah.gueye@laposte.net>
2017-09-06 16:37:12 +0200

    feat(responsive): add responsive

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ function onScroll() {
 }
 
 document.addEventListener('scroll', onScroll);
+
+function onScrollp() {
+    let distance = window.pageYOffset;
+    let target = 300;
+
+    if(distance > target){
+        document.querySelector(".showtext1").style.opacity = "1";
+        document.querySelector(".showtext2").style.opacity = "1";
+    }
+}
+document.addEventListener('scroll', onScrollp);
+
+// togglemenu
+
+function openMenu() {
+    document.querySelector('#side-menu').style.width = '250' + 'px';
+    document.querySelector('.nav-open').style.display = 'none';
+}
+
+function closeMenu() {
+    document.querySelector('#side-menu').style.width = '0' + 'px';
+    document.querySelector('.nav-open').style.display = 'block';
+}
